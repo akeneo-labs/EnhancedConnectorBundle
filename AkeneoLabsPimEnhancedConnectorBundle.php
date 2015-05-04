31f7dab8fe42df16829a8591c077699e4cb471cc
Benoit Jacquemont <benoit@akeneo.com>
2015-05-04 18:45:54 +0200

    First draft of PQB based enhanced product reader

---
+++ AkeneoLabsPimEnhancedConnectorBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AkeneoLabs\Pim\Bundle\EnhancedConnectorBundle;
+
+use Akeneo\Bundle\BatchBundle\Connector\Connector;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Enhanced connector bundle
+ *
+ * @author    Benoit Jacquemont <benoit@akeneo.com>
+ * @copyright 2015 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class AkeneoLabsPimEnhancedConnectorBundle extends Connector
+{
+}
+
